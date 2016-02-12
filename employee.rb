6c52505c2f5084504128b94ed899744c98eb79a5
Daniel Levy <daniellevy@Daniels-MacBook-Pro.local>
2016-02-12 12:06:30 -0500

    test_06 department name

--- employee.rb
+++ employee.rb
@@ -1,7 +1,7 @@
 class Employee
   attr_reader :employee_name, :employee_email_address, :employee_phone_number, :employee_salary
 
-  def initialize(employee_name, employee_email_address, employee_phone_number,employee_salary)
+  def initialize(employee_name:, employee_email_address:, employee_phone_number:, employee_salary:)
     @employee_name = employee_name
     @employee_email_address = employee_email_address
     @employee_phone_number = employee_phone_number
--- employees_reviews_test.rb
+++ employees_reviews_test.rb
@@ -1,6 +1,6 @@
 require 'minitest/autorun'
 require 'minitest/pride'
-#require 'byebug'
+require 'byebug'
 
 
 #Note: This line is going to fail first.
@@ -12,38 +12,43 @@ class EmployeesReviewsTest < Minitest::Test
     assert Department
     a = Department.new("Widgets")
     assert a
-    assert_equal "Widgets", a.department_name
+    assert_equal a.department_name, "Widgets"
   end
 
   def test_02_employee_class_exists
     assert Employee
-    e = Employee.new("Greg", "Greg@yahoo.com", 7179991111, 100000.00)
+    e = Employee.new(employee_name:"Greg", employee_email_address:"Greg@yahoo.com", employee_phone_number:"7179991111", employee_salary:100000.00)
     assert e
     assert_equal "Greg", e.employee_name
     assert_equal "Greg@yahoo.com", e.employee_email_address
-    assert_equal 7179991111, e.employee_phone_number
+    assert_equal "7179991111", e.employee_phone_number
     assert_equal 100000.00, e.employee_salary
 
   end
 
-  def test_03_Employee_Added_To_Department
-   d = Department.new("Blidgets")
-   e = Employee.new("Greg", "Greg@yahoo.com", 7179991111, 100000.00)
+  def test_03_employee_added_to_department
+   d = Department.new(department_name: "Blidgets")
+   e = Employee.new(employee_name:"Greg", employee_email_address:"Greg@yahoo.com", employee_phone_number:"7179991111", employee_salary:100000.00)
    d.add_employee(e)
    assert_equal [e], d.employees
   end
 
-  def test_04_Get_an_Employees_Name
-    assert e = Employee.new("Greg", "Greg@yahoo.com", 7179991111, 100000.00)
-    assert_equal "Greg", e.employee_name
+  def test_04_get_an_employees_name
+    e = Employee.new(employee_name:"Greg", employee_email_address:"Greg@yahoo.com", employee_phone_number:"7179991111", employee_salary:100000.00)
+    assert e
+    assert_equal e.employee_name, "Greg"
   end
 
-  def test_05_Get_an_Employees_Salary
-    assert e = Employee.new("Greg", "Greg@yahoo.com", 7179991111, 100000.00)
+  def test_05_get_an_employees_salary
+    e = Employee.new(employee_name:"Greg", employee_email_address:"Greg@yahoo.com", employee_phone_number:"7179991111", employee_salary:100000.00)
+    assert e
     assert_equal 100000.00, e.employee_salary
   end
 
-
+  def test_06_get_a_departments_name
+    d = Department.new("Blidgets")
+    assert_equal "Blidgets", d.department_name
+  end
 
 
 end
