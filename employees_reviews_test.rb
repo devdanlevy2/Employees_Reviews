3ed8e3e5fad4bbc1f8ba4b2508a1323168f45e22
Daniel Levy <daniellevy@Daniels-MBP.attlocal.net>
2016-02-14 00:17:30 -0500

    test_13 working on part three incorporating a regex algorhythm to assess satisfactory performance

--- employees_reviews_test.rb
+++ employees_reviews_test.rb
@@ -157,4 +157,65 @@ class EmployeesReviewsTest < Minitest::Test
     assert_in_delta 85000.00, g.employee_salary, 0.01
     end
 
+    def test_13_regex_to_determine_satisfactory_rating
+      e = Employee.new(employee_name: "Greg", employee_email_address: "Greg@yahoo.com",
+      employee_phone_number: "7179991111", employee_salary: 80000.00,
+      employee_satisfaction: true, employee_review: "Greg is a very positive person
+      and encourages those around him, but he has not done well technically this year.
+      There are two areas in which Greg has room for improvement.  First, when communicating
+      verbally (and sometimes in writing), he has a tendency to use more words than are required.
+      This conversational style does put people at ease, which is valuable, but it often makes
+      the meaning difficult to isolate, and can cause confusion.
+      Second, when discussing new requirements with project managers, less of the information is
+      retained by Greg long-term than is expected.  This has a few negative consequences:
+      1) time is spent developing features that are not useful and need to be re-run,
+      2) bugs are introduced in the code and not caught because the tests lack the same information, and
+      3) clients are told that certain features are complete when they are inadequate.
+      This communication limitation could be the fault of project management, but given that other
+      developers appear to retain more information, this is worth discussing further.")
+
+      f = Employee.new(employee_name: "Richard", employee_email_address: "Greg@yahoo.com",
+      employee_phone_number: "7179991111", employee_salary: 90000.00, employee_satisfaction: true,
+      employee_review: "Thus far, there have been two concerns over Richard's performance,
+      and both have been discussed with her in internal meetings.  First, in some cases,
+      Richard takes longer to complete tasks than would normally be expected.  This most
+      commonly manifests during development on existing applications, but can sometimes occur
+      during development on new projects, often during tasks shared with Andrew.  In order to
+      accommodate for these preferences, Richard has been putting more time into fewer projects,
+      which has gone well.  Second, while in conversation, Richard has a tendency to interrupt,
+      talk over others, and increase her volume when in disagreement.  In client meetings,
+      she also can dwell on potential issues even if the client or other attendees have
+      clearly ruled the issue out, and can sometimes get off topic.")
+
+      g = Employee.new(employee_name: "Danny", employee_email_address: "Greg@yahoo.com",
+      employee_phone_number: "7179991111", employee_salary: 60000.00, employee_satisfaction: false,
+      employee_review: "Danny is a huge asset to SciMed and is a pleasure to work with.
+      He quickly knocks out tasks assigned to him, implements code that rarely needs to
+      be revisited, and is always willing to help others despite his heavy workload.
+      When Danny leaves on vacation, everyone wishes he didn't have to go  Last year,
+      the only concerns with Danny performance were around ownership.  In the past
+      twelve months, he has successfully taken full ownership of both Acme and Bricks, Inc.
+      Aside from some false starts with estimates on Acme, clients are happy with his work and
+      responsiveness, which is everything that his managers could ask for.")
+
+      h = Employee.new(employee_name: "Molly", employee_email_address: "Molly@yahoo.com",
+      employee_phone_number: "7179991311", employee_salary: 70000.00,
+      employee_satisfaction: true, employee_review: "Molly has been an incredibly consistent
+      and effective developer.  Clients are always satisfied with her work, developers are
+      impressed with her productivity, and she's more than willing to help others even when
+      she has a substantial workload of her own.  She is a great asset to Awesome Company, and
+      everyone enjoys working with her.  During the past year, she has largely been devoted
+      to work with the Cement Company, and she is the perfect woman for the job.  We know that
+      work on a single project can become monotonous, however, so over the next few months,
+      we hope to spread some of the Cement Company work to others.  This will also allow Molly
+      to pair more with others and spread her effectiveness to other projects.")
+
+      d = Department.new("Test_testers_quality_control_department")
+      d.add_employee(e)
+      d.add_employee(f)
+      d.add_employee(g)
+      d.add_employee(h)
+
+    end
+
 end
